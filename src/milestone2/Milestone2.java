9f5777a358e83dbc1f424533c7905e2abd2f8122
raymond <Raymond@10.10.230.106>
2012-09-07 14:24:19 -0700

    added essentials package and created milestone 2 package

---
+++ src/milestone2/Milestone2.java
@@ -0,0 +1,14 @@
+package milestone2;
+import essentials.*;
+
+public class Milestone2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
