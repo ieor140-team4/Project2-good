f3ca59922008cb404c60cb351de3714d3705658d
raymond <Raymond@10.10.230.106>
2012-09-07 14:46:50 -0700

    added more imports

--- src/milestone2/Milestone2.java
+++ src/milestone2/Milestone2.java
@@ -1,13 +1,12 @@
 package milestone2;
 import essentials.*;
+import lejos.robotics.navigation.*;
+import lejos.nxt.*;
 
 public class Milestone2 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Button.waitForAnyPress();
 
 	}
 
