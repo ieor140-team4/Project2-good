4132f49059ab1a52499c0f8b11cc6a16b26789e8
raymond <Raymond@10.10.230.106>
2012-09-11 16:52:07 -0700

    created ButtonInputter for user input.

---
+++ src/essentials/ButtonInputter.java
@@ -0,0 +1,21 @@
+package essentials;
+
+import lejos.nxt.*;
+import lejos.util.*;
+
+public class ButtonInputter {
+
+	private int x, y, focus;
+	
+	void display(){
+		
+	}
+	
+	void scroll(){
+		
+	}
+
+	void save(){
+		
+	}
+}
