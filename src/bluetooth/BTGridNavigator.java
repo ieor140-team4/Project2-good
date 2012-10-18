cac5ad91db92fb051ef2acd11349fe5a91cdf601
nate.kb <nate.kb@quail.ieor.berkeley.edu>
2012-10-18 15:48:44 -0700

    Updated BTGridNavigator.

--- src/bluetooth/BTGridNavigator.java
+++ src/bluetooth/BTGridNavigator.java
@@ -10,12 +10,12 @@ import essentials_new.Tracker;
 public class BTGridNavigator extends GridNavigator {
 
 	private BTCommunicator bt;
-	
+
 	public BTGridNavigator(Tracker tracker, ObstacleDetector detector, BTCommunicator comm) {
 		super(tracker, detector);
 		bt = comm;
 	}
-	
+
 	public Point getDestination() {
 		try {
 			return bt.receive();
@@ -23,5 +23,27 @@ public class BTGridNavigator extends GridNavigator {
 			return null;
 		}
 	}
+
+	public void go() {
+
+		Point destination = null;
+		
+		while (true) {
+
+			while (destination == null) {
+				destination = getDestination();
+			}
+			
+			moveTo(destination.x, destination.y);
+		}
+	}
 	
+	/**
+	 * 
+	 * @return true if the send is a success, false otherwise
+	 */
+	public boolean sendData() {
+		return true;
+	}
+
 }
